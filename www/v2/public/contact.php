49013c37e9153accbba0e99e19b2e1dac2656396
Nur <nurgisa0136@gmail.com>
2024-05-30 11:34:20 +0500

    Сделал Слово Главное ссылку на административную панель

--- www/v2/public/contact.php
+++ www/v2/public/contact.php
@@ -29,7 +29,7 @@ $res = $sql->fetch(PDO::FETCH_ASSOC);
           <div class="col-lg-3 d-none d-lg-block">
             <div class="quick-contact-icons d-flex align-items-center">
               <div class="text">
-                <span class="h4 d-block"> <?php echo $res["city"]; ?> </span>
+                <span class="h4 d-block"> <a href = "http://nur.tech-news-forum.com/admin.php"> <?php echo $res["city"]; ?> </a> </span>
               </div>
             </div>
           </div>
