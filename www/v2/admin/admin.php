8c90950a39fd2a4422b851d0af1ae974e2edcdc8
nurgisavmgooglecloud <nurgisa.sharipov@mail.ru>
2024-05-30 03:07:14 +0000

    Удалил два файла и мелочи

--- www/v2/admin/admin.php
+++ www/v2/admin/admin.php
@@ -1,6 +1,5 @@
 <?php require_once '../function/connect.php' ;?>
 
-<?php session_start();?>
 
 <?php
 $login=$_POST["login"];
@@ -10,11 +9,5 @@ $sql = $pdo->prepare("SELECT id FROM login WHERE login=:login AND password=:pass
 $sql->execute(array('login' =>$login,'password'=>$password));
 $array=$sql->fetch(PDO::FETCH_ASSOC);
 
-if($array["id"]>0) {
-    $_SESSION['login']=$array["login"];
-    header('Location:../admin.php');
-}
-else{
-    header('Location:../login.php');
-}
+
 ?>
