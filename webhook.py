84571afdcdde9ae4315c8d0b7ad07d6987c5315d
nurgisavmgooglecloud <nurgisa.sharipov@mail.ru>
2024-05-30 06:19:09 +0000

    Добавил файл на питоне, для автоматизации

---
+++ webhook.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+import os
+import subprocess
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        # Перейдите в каталог с вашим репозиторием
+        os.chdir('/path/to/your/repository')
+        
+        # Выполните git pull и перезапуск Docker-контейнеров
+        subprocess.run(['git', 'pull', 'origin', 'main'])
+        subprocess.run(['sudo', 'docker-compose', 'down'])
+        subprocess.run(['sudo', 'docker-compose', 'up', '-d'])
+        
+        return 'OK', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
